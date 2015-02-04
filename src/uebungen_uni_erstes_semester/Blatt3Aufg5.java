841e3a5e66ed8bcbb9ee442c76830f31eb43fa5d
TvRXVII <TvRXVII@users.noreply.github.com>
2015-02-04 09:59:46 +0100

    Update Blatt3Aufg5.java

--- src/uebungen_uni_erstes_semester/Blatt3Aufg5.java
+++ src/uebungen_uni_erstes_semester/Blatt3Aufg5.java
@@ -17,20 +17,13 @@ public class Blatt3Aufg5 {
 			// Zahlen in ein Array einfügen
 			for (int i = zahlen.length - 1; i >= 0; i--) {
 				zahlen[zahlen.length - 1 - i] = Integer.parseInt(String.valueOf(Eingabe.charAt(i)));
-				// System.out.println(zahlen[i]);
 			}
 			double sum = 0;
 			for (int i = 0; i < zahlen.length; i++) {
 
 				double Dezimal = Math.pow(2, i) * zahlen[i];
 				sum = sum + Dezimal;
-				// String A = String.valueOf(Dezimal);
-				// int B;
-				// B = Integer.parseInt(A);
-				// String C = String.valueOf(zahl);
-				// int D;
-				// D = Integer.parseInt(C);
-				// int Ausgabe = B * D;
+				
 
 			}
 
