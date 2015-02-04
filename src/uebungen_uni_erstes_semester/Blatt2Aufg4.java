f116d927f22345407b389ef919ea7c6bc773c9f8
TvRXVII <TvRXVII@users.noreply.github.com>
2015-02-04 10:53:16 +0100

    Update Blatt2Aufg4.java

--- src/uebungen_uni_erstes_semester/Blatt2Aufg4.java
+++ src/uebungen_uni_erstes_semester/Blatt2Aufg4.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Blatt2Aufg4 {
+//Lösen der untenstehenden Formel mit den eingebbaren Variablen a und b	
 	public static void main(String[] args) {
 		try {
 			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
