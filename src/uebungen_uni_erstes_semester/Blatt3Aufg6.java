a20ffbf0f3be2b82a9d2d933181057675379ddff
TvRXVII <TvRXVII@users.noreply.github.com>
2015-02-04 10:57:53 +0100

    Update Blatt3Aufg6.java

--- src/uebungen_uni_erstes_semester/Blatt3Aufg6.java
+++ src/uebungen_uni_erstes_semester/Blatt3Aufg6.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Blatt3Aufg6 {
-
+//zwei zahlen werden eingegeben und es wird nur die Summe der beiden Zahlen ausgegeben wenn sie gleich groß sind
 	public static void main(String[] args) {
 		try {
 			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
